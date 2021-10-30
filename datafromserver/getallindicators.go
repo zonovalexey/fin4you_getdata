ecdfcb49a9dea075e4491717ca6647ad75927d6a
Zonov Alexey <zonovalexey@gmail.com>
2021-10-30 22:37:36 +0300

    initial commit

--- datafromserver/getallindicators.go
+++ datafromserver/getallindicators.go
@@ -8,6 +8,6 @@ import (
 func Getall() {
 	data := server_get_api_struct("aapl", "1d")
 	for _, v := range data {
-		fmt.Println(time.Unix(v.date, 0))
+		fmt.Println(time.Unix(v.date, 0), v.open, v.close, (v.close/v.open-1)*100)
 	}
 }
